a06c76d3c23b39019585a896e01ec931d066e04a
Glenford Williams <gw_dev@outlook.com>
2021-10-11 09:07:35 -0500

    log deserializer errors

--- libgbrainy/src/reader/mod.rs
+++ libgbrainy/src/reader/mod.rs
@@ -1,12 +1,21 @@
+use std::borrow::Borrow;
+use std::fmt::{Display, Formatter};
+
 use serde::Deserialize;
 use serde_xml_rs::Deserializer;
+use serde_xml_rs::Error;
 
 use crate::models::game::GameCollection;
 
-pub fn parse_game_data(data: Box<&str>) -> GameCollection {
-    let mut ser=  Deserializer::new_from_reader(data.as_bytes())
+pub fn parse_game_data(data: Box<&str>) -> Option<GameCollection> {
+    let mut ser = Deserializer::new_from_reader(data.as_bytes())
         .non_contiguous_seq_elements(true);
 
-    GameCollection::deserialize(&mut ser).unwrap()
-
+    match GameCollection::deserialize(&mut ser) {
+        Ok(res) => { Some(res) }
+        Err(e) => {
+            log::error!("parse_game_data(): {}", e);
+            None
+        }
+    }
 }
