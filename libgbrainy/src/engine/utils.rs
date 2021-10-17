980ea99f27c935a9dabacf551690d01a94186ff0
Glenford Williams <gw_dev@outlook.com>
2021-10-17 15:27:07 -0500

    add pluralize helper for questions

--- libgbrainy/src/engine/utils.rs
+++ libgbrainy/src/engine/utils.rs
@@ -3,23 +3,26 @@ use regex::Regex;
 pub fn get_variables(val: &str) -> Vec<String> {
     let mut vars = vec![];
 
-    for cap in Regex::new(r"\[([A-Za-z_-])+]")
-        .unwrap()
-        .find_iter(val) {
+    for cap in Regex::new(r"\[([A-Za-z_-])+]").unwrap().find_iter(val) {
         // println!("{:#?}", cap.as_str());
         vars.push(String::from(cap.as_str()));
     }
     vars
 }
 
-pub fn clean_var_name(var: &str) ->String{
-    String::from(var)
-        .replace("[","")
-        .replace("]","")
+pub fn pluralize(singular: &str, plural: &str, count: i32) -> String {
+    if count == 1 {
+        return singular.to_string();
+    }
+    plural.to_string()
+}
+
+pub fn clean_var_name(var: &str) -> String {
+    String::from(var).replace("[", "").replace("]", "")
 }
 
 #[test]
-fn get_variable_count(){
+fn get_variable_count() {
     let src = "[a] [qwerty_] [sadfsd]";
     let vars = get_variables(src);
 
@@ -27,6 +30,6 @@ fn get_variable_count(){
 }
 
 #[test]
-fn clean_var_test(){
+fn clean_var_test() {
     assert_eq!("a", clean_var_name("[a]"))
 }
